9eac34d44e03e3081eece681fddd5e2b03c9c488
chris <chrishethertonweb@gmail.com>
2022-06-29 13:59:45 +0200

    correction, wrong exercise. Fixed

--- solution.js
+++ solution.js
@@ -1,3 +1,7 @@
-console.log("This is an exercise in console logging");
+// console.log("This is an exercise in console logging");
 
-let exercise = "This is the value.";
+// let exercise = "This is the value.";
+// console.log(exercise)
+
+let string = "string";
+console.log(string);
