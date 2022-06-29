168323711c1a688c849b31bdcd81425f65c79356
chris <chrishethertonweb@gmail.com>
2022-06-29 14:21:23 +0200

    until point 9, done

--- solution.js
+++ solution.js
@@ -5,3 +5,18 @@
 
 let string = "string";
 console.log(string);
+
+console.log(string[(1, 5)]);
+
+let firstName = "Top";
+let age = 25;
+let year = 1975;
+
+console.log(firstName, "is", age, "in", year);
+
+let isMarried = true;
+console.log(firstName, "is married:", isMarried);
+
+let programmingLanguage = "JavaScript";
+let isFun = true;
+console.log(programmingLanguage, "is fun:", isFun);
